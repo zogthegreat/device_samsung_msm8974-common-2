7bc715cfb360f5e020b67171eebaca864b2debf6
IKGapirov <IKGapirov@gmail.com>
2018-10-17 17:38:12 +0500

    msm8974-common: Fix for build

--- msm8974.mk
+++ msm8974.mk
@@ -58,10 +58,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
 
-# Advanced Display
-PRODUCT_PACKAGES += \
-    AdvancedDisplay
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
